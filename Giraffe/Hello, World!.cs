8daee45cefd79263c233126f3b1fc209565e0287
porte <porte@DESKTOP-MUA3K0D>
2023-01-12 10:13:17 +0100

    Add project files.

---
+++ Giraffe/Hello, World!.cs
@@ -0,0 +1,12 @@
+﻿namespace Giraffe
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Giraffe/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Giraffe
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            Console.ReadLine();
+        }
+    }
+}
